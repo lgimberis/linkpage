ebc9e8c581b8b2f6132579610fff174780898d9d
Lukas Gimberis <lukasgimberis@gmail.com>
2022-06-21 16:04:14 +0100

    Add an is-authenticated mini-view to confirm whether the user is authenticated

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.core.exceptions import ValidationError
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import api_view
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -26,3 +27,8 @@ class LinkViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+@api_view(['GET'])
+def is_authenticated(request):
+    response = Response({"is_authenticated": request.user.is_authenticated})
+    return response
